0b618be1592d400776167e0ea6068ecba6b3012d
Lizi <86664792+whooslizi@users.noreply.github.com>
2026-04-07 11:46:15 +0700

    feat. added more function

--- DateReminder/CountdownEvent.swift
+++ DateReminder/CountdownEvent.swift
@@ -2,9 +2,6 @@
 //  CountdownEvent.swift
 //  DateReminder
 //
-//  Created by Hodaka on 13/3/25.
-//
-
 
 import Foundation
 
@@ -12,11 +9,22 @@ struct CountdownEvent: Identifiable {
     let id = UUID()
     var name: String
     var date: Date
+    var startDate: Date
 
     var daysLeft: Int {
-        let calendar = Calendar.current
-        let now = Date()
-        let components = calendar.dateComponents([.day], from: now, to: date)
-        return components.day ?? 0
+        let cal = Calendar.current
+        let components = cal.dateComponents([.day], from: Date(), to: date)
+        return max(components.day ?? 0, 0)
+    }
+
+    var hoursLeft: Int {
+        return max(Int(date.timeIntervalSince(Date()) / 3600), 0)
+    }
+
+    var percentLeft: Double {
+        let total     = date.timeIntervalSince(startDate)
+        let remaining = date.timeIntervalSince(Date())
+        guard total > 0 else { return 0 }
+        return max(min(remaining / total * 100.0, 100.0), 0.0)
     }
 }
